287abda413200660686653a240be65f4ed743489
Harshit Yadav <2105891@kiit.ac.in>
2024-11-27 00:27:52 +0530

    feat: Frontend setup

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import CountdownTimer from './components/CountDownTimer/CountDownTimer'
+
+function App() {
+
+  return (
+    <>
+      <CountdownTimer />
+    </>
+  )
+}
+
+export default App
