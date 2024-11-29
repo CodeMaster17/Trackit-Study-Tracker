3ea2122f352440db42d49c19aacc844e219a5fa5
Harshit Yadav <2105891@kiit.ac.in>
2024-11-29 21:56:51 +0530

    feat: Sidebar active route css created

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -10,6 +10,7 @@ interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 export function Sidebar({ className }: SidebarProps) {
+    const currentUrl = window.location.pathname;
     return (
         <div className={cn("pb-12 min-h-screen", className)}>
             <div className="space-y-4 py-4">
@@ -21,8 +22,8 @@ export function Sidebar({ className }: SidebarProps) {
                     <div className="space-y-1">
                         {SidebarRoutes.map((item) => {
                             return (
-                                <Button key={item.id} variant="link" className="w-full justify-start">
-                                    <a href={item.route} className="w-full justify-start inline-flex items-center  gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0">
+                                <Button key={item.id} variant="link" className={`w-full justify-start hover:bg-primary/10 hover:no-underline ${item.route == currentUrl ? "bg-primary/10 text-primary" : ""}`}>
+                                    <a href={item.route} className="w-full justify-start inline-flex items-center  gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors  disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 hover:no-underline">
                                         {item.icon}
                                         {item.name}
                                     </a>
