6b227ad5acfd32e3b259cbd3692777b316d9a72e
Harshit Yadav <2105891@kiit.ac.in>
2024-11-30 13:08:05 +0530

    Resources route added

---
+++ client/src/pages/Dashboard/pages/Resources.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button"
+
+const Resources = () => {
+    return (
+        <div className="w-full">
+            <div className="w-full flex justify-between">
+                <p className="text-xl">
+                    Resources
+                </p>
+                <Button>Add +</Button>
+            </div>
+        </div>
+    )
+}
+
+export default Resources
